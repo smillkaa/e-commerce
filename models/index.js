f7ea0eae196c83b81b235dd278a332dd4e27079f
Seleste <104727784+smillkaa@users.noreply.github.com>
2022-08-22 19:59:08 -0700

    created associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,19 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  onDelete: 'CASCADE'
+})
 // Categories have many Products
+Category.hasMany(Product, {
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag })
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag })
 
 module.exports = {
   Product,
